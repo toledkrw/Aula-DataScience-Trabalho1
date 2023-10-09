80da14ad01c5544108a10508193dfc6f3414cb08
Matheus <matheus.tererj@hotmail.com>
2023-10-09 14:59:35 -0300

    ✅ added utils tests

---
+++ .test/JsonUtils_test.py
@@ -0,0 +1,46 @@
+import unittest, sys, os, shutil
+sys.path.append(os.getcwd())
+
+from utils.JsonUtils import JsonUtils
+
+
+class tests(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.path = "data/test"
+
+
+    def cleanUp(self):
+        if (not os.path.exists(self.path)):
+            return
+
+        shutil.rmtree(self.path)
+
+    def createPath(self):
+        if (not os.path.exists(self.path)):
+            os.makedirs(self.path)
+
+
+    def setUp(self):
+        self.createPath()
+
+    def tearDown(self):
+        self.cleanUp()
+        
+
+    def test_writeJson(self):
+        test_data = {"test": "test"}
+        
+        JsonUtils.writeJson(test_data, self.path + "/test.json")
+        
+        file = os.listdir(self.path)
+        self.assertEqual(len(file), 1)
+        
+        
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(tests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+main()
---
+++ .test/PathUtils_test.py
@@ -0,0 +1,48 @@
+import unittest, sys, os, shutil
+sys.path.append(os.getcwd())
+
+from utils.PathUtils import PathUtils
+
+
+class tests(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.path = "data/test"
+
+
+    def cleanUp(self):
+        if (not os.path.exists(self.path)):
+            return
+
+        shutil.rmtree(self.path)
+
+
+    def setUp(self):
+        self.cleanUp()
+
+    def tearDown(self):
+        self.cleanUp()
+
+    
+    def test_checkPath(self):
+        self.assertFalse(PathUtils.checkPath(self.path))
+        
+        os.makedirs(self.path)
+        
+        self.assertTrue(PathUtils.checkPath(self.path))
+
+    def test_createPath(self):
+        self.assertFalse(PathUtils.checkPath(self.path))
+        
+        PathUtils.createPath(self.path)
+        
+        self.assertTrue(PathUtils.checkPath(self.path))
+        
+        
+
+def main():
+    suite = unittest.TestLoader().loadTestsFromTestCase(tests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+main()
