9b03de53b6d513cf159925e161f72b6fa9d8d5cb
Matheus <matheus.tererj@hotmail.com>
2023-10-09 16:29:55 -0300

    👔 Updated the wat an empty query should work

--- market/search.py
+++ market/search.py
@@ -1,6 +1,6 @@
 import requests
 
-def search_market_items(appid, query="%20"):
+def search_market_items(appid, query=" "):
     url = f"https://steamcommunity.com/market/search/render/?query={query}&appid={appid}&norender=1&county=BR&currency=7&l=portuguese&start=0&count=100"
     
     response = requests.get(url)
