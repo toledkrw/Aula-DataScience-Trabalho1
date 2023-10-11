5a89f1af99a99eca9bae55144382631440910dd9
Matheus <matheus.tererj@hotmail.com>
2023-10-10 21:36:52 -0300

    👔 Updated Request URL string

--- market/search.py
+++ market/search.py
@@ -1,7 +1,7 @@
 import requests
 
 def search_market_items(appid, query=" "):
-    url = f"https://steamcommunity.com/market/search/render/?query={query}&appid={appid}&norender=1&county=BR&currency=7&l=portuguese&start=0&count=100"
+    url = f"https://steamcommunity.com/market/search/render/?norender=1&country=BR&currency=7&start=0&count=100&appid={appid}&q={query}"
     
     response = requests.get(url)
     data = response.json()
