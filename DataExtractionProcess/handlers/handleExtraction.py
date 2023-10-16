24cae5d9f12583626e99aece068d82991e78eb0d
Matheus <matheus.tererj@hotmail.com>
2023-10-15 21:49:31 -0300

    🚚 renamed handleSearch to handleExtraction

--- DataExtractionProcess/handlers/handleExtraction.py
+++ DataExtractionProcess/handlers/handleExtraction.py
@@ -1,19 +1,19 @@
-def handleSearch(app_id, query):
+def handleExtraction(app_id, query):
     if(app_id == None):
         raise Exception("App ID is None")
     else:
         try:
             from datetime import datetime
-            from market import search
-            from utils.JsonUtils import JsonUtils
-            from utils.PathUtils import PathUtils
+            from DataExtractionProcess.market import search
+            from DataExtractionProcess.utils.JsonUtils import JsonUtils
+            from DataExtractionProcess.utils.PathUtils import PathUtils
 
             data = search.search_market_items(app_id, query)
             
             if(data == None):
                 raise Exception("Data is empty!")
             else:
-                path = "../DATA/RAW/" + str(app_id) 
+                path = "./DATA/RAW/" + str(app_id) 
 
                 file_name = str(datetime.now())
                 file_name = file_name.replace(" ", "_").replace(".",":").replace(":","-") + ".json" 
